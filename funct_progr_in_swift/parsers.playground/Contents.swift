08a33ec490dc29b8f4cc2b2859122709987322a7
littleandroide <jfragoso.perez@gmail.com>
2015-06-10 14:43:20 +0200

    parsers

--- funct_progr_in_swift/parsers.playground/Contents.swift
+++ funct_progr_in_swift/parsers.playground/Contents.swift
@@ -10,6 +10,51 @@ We will use sequences and slices. We define a parser as a function that takes a 
 processes some of these tokens, and returns a tuple of the result and the remainder of the tokens.
 **/
 
+
+extension String {
+    var characters: [Character] {
+        var result: [Character] = []
+        for c in self {
+            result += [c]
+        }
+        return result
+    }
+    var slice: ArraySlice<Character> {
+        let res = self.characters
+        return res[0..<res.count]
+    }
+}
+
+extension ArraySlice {
+    var head: T? {
+        return self.isEmpty ? nil : self[0]
+    }
+    
+    var tail: ArraySlice<T> {
+        if (self.isEmpty) {
+            return self
+        }
+        return self[(self.startIndex+1)..<self.endIndex]
+    }
+    
+    var decompose: (head: T, tail: ArraySlice<T>)? {
+        return self.isEmpty ? nil
+            : (self[self.startIndex], self.tail)
+    }
+}
+
+extension Character: Printable {
+    public var description: String {
+        return "\"(self)\""
+    }
+}
+
+func string(characters: [Character]) -> String {
+    var s = ""
+    s.extend(characters)
+    return s
+}
+
 struct Parser<Token, Result> {
     let p: ArraySlice<Token> -> SequenceOf<(Result, ArraySlice<Token>)>
 }
@@ -24,6 +69,13 @@ func parseCharacter(character: Character) -> Parser<Character, Character> {
         }
         return none()
     }
+}
+
+func none<A>() -> SequenceOf<A> {
+    return SequenceOf(GeneratorOf { nil } )
+}
+func one<A>(x: A) -> SequenceOf<A> {
+    return SequenceOf(GeneratorOfOne(x))
 }
 
 //we can abstract this method one final time, making it generic over any kind of token.
@@ -46,3 +98,21 @@ func satisfy<Token>(condition: Token -> Bool) -> Parser<Token, Token> {
 func token<Token: Equatable>(t: Token) -> Parser<Token, Token> {
     return satisfy { $0 == t }
 }
+
+//parsing a single symbol isn't very useful, unless we add functions to combine
+//two parsers.
+//the first function we introduce is choice operator, and it can parse using either the left
+//operand or the right operand. It is implemented in a simple way: given an input string,
+//it runs the left operand's parser, which yelds a sequence of possible results. Then it runs 
+//the right operand, which also yields a sequence of possible resoluts, and it concatenates the two sequences.
+
+infix operator <|> {associativity right precedence 130 }
+func <|> <Token, A> (l: Parser<Token, A>, r: Parser<Token, A>) -> Parser<Token, A> {
+    return Parser {
+        input in l.p(input) + r.p(input)
+    }
+}
+
+
+
+
